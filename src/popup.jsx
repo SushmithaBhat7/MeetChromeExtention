a4ad198c24893630dd7cf75e2b35d31a2e1bac5c
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2023-12-29 09:05:03 +0530

    Added PDF generate functionality

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import jsPDF from "jspdf";
 import CaptionsViewer from "./captionsViewer";
 
 const PopupComponentM = () => {
@@ -42,6 +43,59 @@ const PopupComponentM = () => {
     setViewCapture(!viewCapture);
   };
 
+  const handleDownloadPDF = () => {
+    // Create a new jsPDF instance
+    const pdf = new jsPDF();
+
+    const combinedCaptions = [];
+    for (let caption of captions) {
+      caption = caption.replace(/\n/g, ""); // Remove line breaks before adding to lastCaption
+
+      if (combinedCaptions.length === 0) {
+        combinedCaptions.push(caption);
+      } else {
+        const lastCaption = combinedCaptions[
+          combinedCaptions.length - 1
+        ].replace(/\n/g, "");
+        if (
+          caption.startsWith(lastCaption) ||
+          lastCaption.startsWith(caption) ||
+          caption.toLowerCase().slice(0, -1) ===
+            lastCaption.toLowerCase().slice(0, -1)
+        ) {
+          // If the new caption starts with the previous one or vice versa, combine them.
+          combinedCaptions[combinedCaptions.length - 1] = caption;
+        } else {
+          // If not, push the new caption to the array.
+          combinedCaptions.push(caption);
+        }
+      }
+    }
+
+    pdf.addFileToVFS("./assets/hindi.TTF", "HindiFont");
+    pdf.addFont("./assets/hindi.TTF", "HindiFont", "normal");
+
+    const addMultilineText = (textArray, font, x, y, maxWidth) => {
+      pdf.setFont(font);
+      pdf.setFontType("normal");
+      textArray.forEach((text, index) => {
+        // Split the text into lines based on the maxWidth
+        const lines = pdf.splitTextToSize(text, maxWidth);
+
+        // Add each line to the PDF
+        lines.forEach((line, lineIndex) => {
+          pdf.text(line, x, y + index * 10 + lineIndex * 10); // Adjust the line height as needed
+        });
+      });
+    };
+
+    // Add multiline text to the PDF
+    addMultilineText(combinedCaptions, "HindiFont", 10, 10, 180); // Adjust the maxWidth as needed
+
+    // Save the PDF as a file
+    pdf.save("multiline_text.pdf");
+  };
+
   sendMessageToMainPage();
 
   return (
@@ -59,6 +113,7 @@ const PopupComponentM = () => {
         ) : (
           <button onClick={handleButtonClickViewCapture}>View Capture</button>
         )}
+        <button onClick={handleDownloadPDF}>PDF Download</button>
       </div>
     </div>
   );
