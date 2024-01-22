9753267f032412207351668588aceae848d17cf4
SushmithaBhat7 <sushmitha.bhat7@gmail.com>
2024-01-22 12:05:02 +0530

    Optimised code using minification and code splitting

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,27 @@ import { crx } from "@crxjs/vite-plugin";
 
 import manifest from "./manifest.json";
 
+import viteCompression from "vite-plugin-compression";
+import { splitVendorChunkPlugin } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
 // const manifest = manifestJson as ManifestV3Export;
 
 export default defineConfig({
-  plugins: [jsconfigPaths(), react(), crx({ manifest })],
+  plugins: [
+    jsconfigPaths(),
+    react(),
+    crx({ manifest }),
+    viteCompression({ algorithm: "brotliCompress" }),
+    splitVendorChunkPlugin(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  build: {
+    sourcemap: false,
+    cssCodeSplit: true,
+    minify: true,
+    chunkSizeWarningLimit: 850,
+  },
 });
